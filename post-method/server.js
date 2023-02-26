b46c773fbd18ea34fcab4fda351b2700bca26a6f
deynsiee <123538400+deynsiee@users.noreply.github.com>
2023-02-26 21:44:37 +0800

    2 files modified

--- post-method/server.js
+++ post-method/server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
-//require body-parser for this method
+//require the body parser for this method
 var bodyParser = require('body-parser');
+const { urlencoded, response } = require('express');
 
 //Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -11,12 +12,13 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/' + 'index.html');
 });
 
-app.post('/process_post', urlencodedParser, function (req, res) {
-  //Prepare output in JSON Format
+app.post('process_post', urlencodedParser, function (req, res) {
+  //Prepare output in JSON format
   response = {
     first_name: req.body.first_name,
     last_name: req.body.last_name,
   };
+
   console.log(response);
   res.end(JSON.stringify(response));
 });
